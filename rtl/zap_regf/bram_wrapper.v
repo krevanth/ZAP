8530f2ff242f1d599c0981609abc1339c0c573c3
krevanth <revanth91kamaraj@gmail.com>
2016-10-01 10:24:38 +0530

    added fpga support via define

--- rtl/zap_regf/bram_wrapper.v
+++ rtl/zap_regf/bram_wrapper.v
@@ -1,33 +1,41 @@
 /*
  * Synthesizes to a 4 read port, 2 write port RAM and supports up to 64 entries.
- * Used to construct the register file.
+ * Used to construct the register file. Do not use for ASIC.
  */
 
 module bram_wrapper
 (
         input wire              i_clk,
         input wire              i_clk_2x,
+
         input wire              i_reset,
 
         input wire              i_wen,
 
-        input wire  [5:0]       i_wr_addr_a, i_wr_addr_b,       // 2 write addresses.
-        input wire  [31:0]      i_wr_data_a, i_wr_data_b,       // 2 write data.
+        input wire  [5:0]       i_wr_addr_a, 
+                                i_wr_addr_b,       // 2 write addresses.
 
-        input wire  [5:0]       i_rd_addr_a, i_rd_addr_b, i_rd_addr_c, i_rd_addr_d,
-        output reg  [31:0]      o_rd_data_a, o_rd_data_b, o_rd_data_c, o_rd_data_d
+        input wire  [31:0]      i_wr_data_a, 
+                                i_wr_data_b,       // 2 write data.
+
+        input wire  [5:0]       i_rd_addr_a, 
+                                i_rd_addr_b, 
+                                i_rd_addr_c, 
+                                i_rd_addr_d,
+
+        output reg  [31:0]      o_rd_data_a,
+                                o_rd_data_b, 
+                                o_rd_data_c, 
+                                o_rd_data_d
 );
 
 localparam READ_PH  = 1'd0;
 localparam WRITE_PH = 1'd1;
 
 reg phase;
-
+reg wen;
 reg  [5:0]  addr_a      [3:0];
 reg  [5:0]  addr_b      [3:0];
-reg  [3:0]  wen;
-reg  [31:0] wr_data_a   [3:0];
-reg  [31:0] wr_data_b   [3:0];
 wire [31:0] rd_data     [3:0];
 
 always @*
@@ -48,7 +56,6 @@ begin: blk1
         if ( phase == READ_PH )
         begin
                 wen             = 4'd0;    
- 
                 addr_a[0]       = i_rd_addr_a;
                 addr_a[1]       = i_rd_addr_b;
                 addr_a[2]       = i_rd_addr_c;
@@ -56,7 +63,7 @@ begin: blk1
         end
         else
         begin
-                wen             = 4'd1;
+                wen             = i_wen;
 
                 for(i=0;i<4;i=i+1)
                 begin
@@ -73,9 +80,9 @@ generate
                 .i_clk_2x(i_clk_2x), 
                 .i_addr_a(addr_a[gi]), 
                 .i_addr_b(addr_b[gi]), 
-                .i_wen(wen[gi]), 
-                .i_wr_data_a(wr_data_a[gi]), 
-                .i_wr_data_b(wr_data_b[gi]), 
+                .i_wen(wen), 
+                .i_wr_data_a(i_wr_data_a), 
+                .i_wr_data_b(i_wr_data_b), 
                 .o_rd_data_a(rd_data[gi])
         );
         end
