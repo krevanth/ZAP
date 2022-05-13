da4e9853d1b3ab4afdc5d3d19a02c87b8e6226c9
krevanth <revanth91kamaraj@gmail.com>
2022-05-14 05:18:01 +0530

    Better verilator performance
    Former-commit-id: 34b241f61f1deace025d0888dec97f6adc406f3a

--- src/testbench/zap_test.v
+++ src/testbench/zap_test.v
@@ -46,7 +46,12 @@ module zap_test (
         input  wire            i_wb_ack,
         input  wire    [31:0]  i_wb_dat,
 
-        input  wire    [7:0]   i_mem [65536-1:0]
+        input  wire    [7:0]   i_mem [65536-1:0],
+
+        output wire            UART_SR_DAV_0, 
+        output wire            UART_SR_DAV_1, 
+        output wire    [7:0]   UART_SR_0, 
+        output wire    [7:0]   UART_SR_1
 );
 
 initial
@@ -127,8 +132,10 @@ begin
 end
 
 // UART TX related. Data out of core.
-uart_tx_dumper u_uart_tx_dumper_dev0 ( .i_clk(i_clk), .i_line(o_uart[0]) );
-uart_tx_dumper u_uart_tx_dumper_dev1 ( .i_clk(i_clk), .i_line(o_uart[1]) );
+uart_tx_dumper u_uart_tx_dumper_dev0 (  .i_clk(i_clk), .i_line(o_uart[0]), 
+                                        .UART_SR_DAV(UART_SR_DAV_0), .UART_SR(UART_SR_0) );
+uart_tx_dumper u_uart_tx_dumper_dev1 (  .i_clk(i_clk), .i_line(o_uart[1]), 
+                                        .UART_SR_DAV(UART_SR_DAV_1), .UART_SR(UART_SR_1) );
 
 // DUT
 chip_top #(
@@ -892,93 +899,78 @@ end
 
 endmodule // vic
 
-module uart_tx_dumper ( input wire i_clk, input wire i_line ); 
+module uart_tx_dumper ( input wire i_clk, input wire i_line, 
+                        output reg UART_SR_DAV = 1'd0, output reg [7:0] UART_SR = 1'd0 ); 
 
 localparam UART_WAIT_FOR_START = 0;
 localparam UART_RX             = 1;
 localparam UART_STOP_BIT       = 2;
 
 integer                 uart_state   = UART_WAIT_FOR_START;
-reg                     uart_sof     = 1'd0;
-reg                     uart_eof     = 1'd0;
 integer                 uart_ctr     = 0;
-integer                 uart_bit_ctr = 1'dx;
+integer                 uart_bit_ctr = 0;
 reg [7:0]               uart_sr      = 0;
-reg [7:0]               UART_SR      = 0;
-reg                     UART_SR_DAV  = 0;
 wire                    uart;
-integer    signed       fh;
+integer signed          fh;
 
 assign uart = i_line;
 
 always @ ( posedge i_clk )
 begin
-        UART_SR_DAV = 1'd0;
-        uart_sof = 1'd0;
-        uart_eof = 1'd0;
+        UART_SR_DAV <= 1'd0;
 
         case ( uart_state ) 
                 UART_WAIT_FOR_START:
                 begin
                         if ( !uart ) 
                         begin
-                                uart_ctr = uart_ctr + 1;
-                                uart_sof = 1'd1;
+                                uart_ctr <= uart_ctr + 1;
                         end
 
-                        if ( !uart && uart_ctr == 16  ) 
+                        if ( !uart && (uart_ctr + 1 == 16) ) 
                         begin
-                                uart_sof     = 1'd0;
-                                uart_state   = UART_RX;
-                                uart_ctr     = 0;
-                                uart_bit_ctr = 0;
+                                uart_state   <= UART_RX;
+                                uart_ctr     <= 0;
+                                uart_bit_ctr <= 0;
                         end                        
                 end
 
                 UART_RX:
                 begin
-                        uart_ctr = uart_ctr + 1;
+                        uart_ctr <= uart_ctr + 1;
 
-                        if ( uart_ctr == 2 ) 
-                                uart_sr = uart_sr >> 1 | uart << 7;                                
+                        if ( uart_ctr + 1 == 2 ) 
+                                uart_sr <= uart_sr >> 1 | i_line << 7;                                
 
-                        if ( uart_ctr == 16 ) 
+                        if ( uart_ctr + 1 == 16 ) 
                         begin
-                                uart_bit_ctr = uart_bit_ctr + 1;
-                                uart_ctr = 0;
+                                uart_bit_ctr <= uart_bit_ctr + 1;
+                                uart_ctr     <= 0;
 
-                                if ( uart_bit_ctr == 8 ) 
+                                if ( uart_bit_ctr + 1 == 8 ) 
                                 begin
-                                        uart_state  = UART_STOP_BIT;                               
-                                        UART_SR     = uart_sr;
-                                        UART_SR_DAV = 1'd1;
-                                        uart_ctr    = 0;
-                                        uart_bit_ctr = 0;
+                                        uart_state  <= UART_STOP_BIT;                               
+                                        UART_SR     <= uart_sr;
+                                        UART_SR_DAV <= 1'd1;
+                                        uart_ctr    <= 0;
+                                        uart_bit_ctr<= 0;
                                 end
                         end                        
                 end
 
                 UART_STOP_BIT:
                 begin
-                        uart_ctr = uart_ctr + 1;
+                        uart_ctr <= uart_ctr + 1;
 
-                        if ( uart && uart_ctr == 16 ) // Stop bit.
+                        if ( uart && (uart_ctr + 1 == 16) ) // Stop bit.
                         begin
-                                uart_state      = UART_WAIT_FOR_START;                                
-                                uart_bit_ctr    = 0;
-                                uart_ctr        = 0;
+                                uart_state      <= UART_WAIT_FOR_START;                                
+                                uart_bit_ctr    <= 0;
+                                uart_ctr        <= 0;
                         end
                 end
         endcase
 end
 
-always @ (negedge i_clk)
-begin
-        if ( UART_SR_DAV )
-        begin
-                $write("%c", UART_SR);
-        end
-end
-
-endmodule 
+endmodule // uart_tx_dumper
 
