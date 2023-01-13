a7c5f2e5d992b978e2768d9170e8fd9f19b2a7e0
Revanth Kamaraj <revanth91kamaraj@gmail.com>
2023-01-14 04:27:09 +0530

    Cleaned up decompile.

--- src/rtl/zap_defines.svh
+++ src/rtl/zap_defines.svh
@@ -144,21 +144,4 @@
 `define ZAP_SPAGE_TLB_WDT         (36 + (32-$clog2(SPAGE_TLB_ENTRIES)-12))
 `define ZAP_FPAGE_TLB_WDT         (32 + (32-$clog2(FPAGE_TLB_ENTRIES)-10))
 
-// ----------------------------------------------------------------------------
-
-`define ZAP_DECOMPILE_CCC            cond_code(i_instruction[31:28])
-`define ZAP_DECOMPILE_CRB            arch_reg_num({i_instruction[`ZAP_DP_RB_EXTEND], i_instruction[`ZAP_DP_RB]})
-`define ZAP_DECOMPILE_CRD            arch_reg_num({i_instruction[`ZAP_DP_RD_EXTEND], i_instruction[`ZAP_DP_RD]})
-`define ZAP_DECOMPILE_CRD1           arch_reg_num({i_instruction[`ZAP_SRCDEST_EXTEND], i_instruction[`ZAP_SRCDEST]})
-`define ZAP_DECOMPILE_CRN            arch_reg_num({i_instruction[`ZAP_DP_RA_EXTEND], i_instruction[`ZAP_DP_RA]})
-`define ZAP_DECOMPILE_CRN1           arch_reg_num({i_instruction[`ZAP_BASE_EXTEND], i_instruction[`ZAP_BASE]})
-`define ZAP_DECOMPILE_CRM            arch_reg_num({i_instruction[`ZAP_DP_RB_EXTEND], i_instruction[`ZAP_DP_RB]});
-`define ZAP_DECOMPILE_COPCODE        get_opcode({i_instruction[`ZAP_OPCODE_EXTEND], i_instruction[24:21]})
-`define ZAP_DECOMPILE_CSHTYPE        get_shtype(i_instruction[6:5])
-`define ZAP_DECOMPILE_CRS            arch_reg_num(i_instruction[11:8]);
-`define ZAP_DECOMPILE_XUMULL         3'b100
-`define ZAP_DECOMPILE_XUMLAL         3'b101
-`define ZAP_DECOMPILE_XSMULL         3'b110
-`define ZAP_DECOMPILE_XSMLAL         3'b111
-
 `endif
