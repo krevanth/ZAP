478ca6a4038bc113ee8973d169a4f0e32dfe9fa0
Revanth Kamaraj <revanth91kamaraj@gmail.com>
2023-01-14 10:04:13 +0530

    lic upd

--- src/rtl/zap_mode16_decoder_main.sv
+++ src/rtl/zap_mode16_decoder_main.sv
@@ -1,31 +1,25 @@
-// -----------------------------------------------------------------------------
-// --                                                                         --
-// --    (C) 2016-2022 Revanth Kamaraj (krevanth)                             --
-// --                                                                         --
-// -- --------------------------------------------------------------------------
-// --                                                                         --
-// -- This program is free software; you can redistribute it and/or           --
-// -- modify it under the terms of the GNU General Public License             --
-// -- as published by the Free Software Foundation; either version 3          --
-// -- of the License, or (at your option) any later version.                  --
-// --                                                                         --
-// -- This program is distributed in the hope that it will be useful,         --
-// -- but WITHOUT ANY WARRANTY; without even the implied warranty of          --
-// -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           --
-// -- GNU General Public License for more details.                            --
-// --                                                                         --
-// -- You should have received a copy of the GNU General Public License       --
-// -- along with this program; if not, write to the Free Software             --
-// -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA           --
-// -- 02110-1301, USA.                                                        --
-// --                                                                         --
-// -----------------------------------------------------------------------------
-// --                                                                         --
-// --  Implements a 16-bit instruction decoder. The 16-bit instruction set is --
-// --  not logically organized so as to save on encoding and thus the code    --
-// --  seem a bit complex.                                                    --
-// --                                                                         --
-// -----------------------------------------------------------------------------
+//
+// (C) 2016-2022 Revanth Kamaraj (krevanth)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+// Implements a 16-bit instruction decoder. The 16-bit instruction set is
+// not logically organized so as to save on encoding and thus the code
+// seem a bit complex.
+//
 
 
 
--- src/rtl/zap_predecode_coproc.sv
+++ src/rtl/zap_predecode_coproc.sv
@@ -1,32 +1,26 @@
-// ---------------------------------------------------------------------------
-// --                                                                       --
-// --    (C) 2016-2022 Revanth Kamaraj (krevanth)                           --
-// --                                                                       --
-// -- ------------------------------------------------------------------------
-// --                                                                       --
-// -- This program is free software; you can redistribute it and/or         --
-// -- modify it under the terms of the GNU General Public License           --
-// -- as published by the Free Software Foundation; either version 3        --
-// -- of the License, or (at your option) any later version.                --
-// --                                                                       --
-// -- This program is distributed in the hope that it will be useful,       --
-// -- but WITHOUT ANY WARRANTY; without even the implied warranty of        --
-// -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         --
-// -- GNU General Public License for more details.                          --
-// --                                                                       --
-// -- You should have received a copy of the GNU General Public License     --
-// -- along with this program; if not, write to the Free Software           --
-// -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA         --
-// -- 02110-1301, USA.                                                      --
-// --                                                                       --
-// ---------------------------------------------------------------------------
-// --                                                                       --
-// -- Implements a simple coprocessor interface for the ZAP core. The i/f   --
-// -- is low bandwidth and thus is suited only for coprocessor that do not  --
-// -- perform large data exchanges. Note that the translate function must be--
-// -- present in the coprocessor to account for CPU modes.                  --
-// --                                                                       --
-// ---------------------------------------------------------------------------
+//
+// (C) 2016-2022 Revanth Kamaraj (krevanth)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+// Implements a simple coprocessor interface for the ZAP core. The i/f
+// is low bandwidth and thus is suited only for coprocessor that do not
+// perform large data exchanges. Note that the translate function must be
+// present in the coprocessor to account for CPU modes.
+//
 
 
 
